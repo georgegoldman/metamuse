65c6d1476343f810cc9d65e3e4960f6affe9da87
georgegoldman <georgegoldmanjohn.o@gmail.com>
2024-10-29 15:04:46 +0100

    passed some test

---
+++ src/NFT.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721 {
+
+    uint256 public currentTokenId;
+
+    // The following will create an ERC721 Token called Lisk.
+    constructor() ERC721("Lisk", "LSK") {}
+
+    // mint function
+    function mint(address recipient) public payable returns (uint256) {
+        uint256 newItemId = ++currentTokenId;
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
+}
